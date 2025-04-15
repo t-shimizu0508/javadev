1522a172fe2f1c6e05e32e0f5d742c6ff6b98809
t-shimizu <fko2347091@stu.o-hara.ac.jp>
2025-04-15 14:58:17 +0900

    add bean

---
+++ src/bean/User.java
@@ -0,0 +1,13 @@
+package bean;
+
+public class User implements java.io.Serializable {
+	private boolean isAuthenticated;
+
+	public boolean isAuthenticated() {
+		return isAuthenticated;
+	}
+
+	public void setAuthenticated(boolean isAuthenticated) {
+		this.isAuthenticated = isAuthenticated;
+	}
+}
